5205e3e63a90f03ee188495a07f9b2969b879aa1
wesri <wesri@users.noreply.github.com>
2015-08-27 14:45:42 +0300

    Update TweetUpload.java
    Add missing package name.

--- TweetUpload.java
+++ TweetUpload.java
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
  
 */
+package azure-ul;
 
 import java.io.*;
 import com.microsoft.azure.storage.*;
